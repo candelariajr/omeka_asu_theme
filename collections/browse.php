16fd6ade13ab00051c8ac0e77bb52857abd972ba
Jonathan Candelaria <candelariajr@appstate.edu>
2018-07-02 09:41:23 -0400

    Update browse.php

--- collections/browse.php
+++ collections/browse.php
@@ -1,8 +1,30 @@
+COLLECTIONS\BROWSE.PHP
 <?php
-/**
- * Created by PhpStorm.
- * User: Jonathan
- * Date: 4/5/2018
- * Time: 5:59 AM
- */
-echo "<h1>COLLECTIONS/BROWSE.PHP</h1>";?>
+    $pageTitle = __('Browse Collections collections\browse.php');
+    echo head(array('title'=>$pageTitle, 'bodyid' => 'collections', 'bodyclass' => 'browse'));
+?>
+<?php echo $pageTitle; ?>
+<?php echo __('(%s total)', $total_results); ?>
+<?php foreach (loop('collections') as $collection): ?>
+    <?php if ($collectionImage = record_image('collection', 'square_thumbnail')): ?>
+        <?php echo link_to_collection($collectionImage, array('class' => 'image collection-img')); ?>
+    <?php endif; ?>
+    <?php echo link_to_collection(); ?>
+    <?php if (metadata('collection', array('Dublin Core', 'Description'))): ?>
+         <?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet'=>150))); ?>
+    <?php endif; ?>
+    <?php echo link_to_items_browse(__('View the items in %s', metadata('collection', array('Dublin Core', 'Title'))),
+        array('collection' => metadata('collection', 'id'))); ?>
+    <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
+<?php endforeach; ?>
+<?php echo pagination_links(); ?>
+<?php fire_plugin_hook('public_collections_browse', array('collections'=>$collections, 'view' => $this)); ?>
+<?php if (get_theme_option('Display Featured Collection')): ?>
+    <?php echo __('Featured Collection'); ?>
+    <?php echo random_featured_collection(); ?>
+<?php endif; ?>
+<?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
+    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
+<?php endif; ?>
+END COLLECTIONS\BROWSE.PHP
+<?php echo foot(); ?>
