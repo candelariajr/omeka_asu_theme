33fbc15d196b5e7ae0f9e4eb13c15e8902f52506
Jonathan Candelaria <candelariajr@appstate.edu>
2020-11-13 18:42:42 -0500

    Update search.php

--- items/search.php
+++ items/search.php
@@ -3,15 +3,33 @@ $pageTitle = __('Search Items');
 echo head(array('title' => $pageTitle,
            'bodyclass' => 'items advanced-search'));
 ?>
-ITEMS\SEARCH.PHP
-<h1><?php echo $pageTitle; ?></h1>
-
-<nav class="search-navigation">
-    <?php echo public_nav_items(); ?>
-</nav>
+<div class="header-subtext">
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <h4 class="header-subtext-text-padding"><?php echo $pageTitle; ?></h4>
+            </div>
+            <div class="col-7">
+                <nav class="search-navigation">
+                    <?php echo public_nav_items(); ?>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
 <br/><br/>
-<?php echo $this->partial('items/search-form.php',
-    array('formAttributes' =>
-        array('id'=>'advanced-search-form'))); ?>
-END ITEMS\SEARCH.PHP
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <?php echo $this->partial('items/search-form.php',
+                array('formAttributes' =>
+                    array('id'=>'advanced-search-form'))); ?>
+        </div>
+        <script>
+            $(".field").each(function(){
+                //this.addClass("form-group");
+            });
+        </script>
+    </div>
+</div>
 <?php echo foot(); ?>
