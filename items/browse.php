2f3d05b89abc1d2d2dc9f02f882f66747b198a25
Jonathan Candelaria <candelariajr@appstate.edu>
2020-12-11 12:03:36 -0500

    Update browse.php

--- items/browse.php
+++ items/browse.php
@@ -1,7 +1,6 @@
 <?php
 $pageTitle = __('Browsing Items');
-echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
-?>
+echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse')); ?>
 <div class="header-subtext">
     <div class="container">
         <div class="row">
@@ -30,40 +29,38 @@ $sortLinks[__('Date Added')] = 'added';
 <!--
 These are the sortlinks. They have been removed for some reason.
 <div id="sort-links">
-    <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
+    <span class="sort-label"><?php //echo __('Sort by: '); ?></span><?php //echo browse_sort_links($sortLinks); ?>
 </div>-->
 
 <?php endif; ?>
 <div class="container">
-<div class="card-columns">
-<?php foreach (loop('items') as $item): ?>
-    <div class="card">
-        <h2 class="card-title"><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
-        <hr>
-        <?php if (metadata('item', 'has files')): ?>
-            <div class="card-img-top">
-                <?php echo link_to_item(item_image('square_thumbnail')); ?>
-            </div>
-        <?php endif; ?>
-
-        <div class="card-body">
-            <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
-                <div class="card-text item-description">
-                    <?php echo $description; ?>
+    <div class="card-columns">
+    <?php foreach (loop('items') as $item): ?>
+        <div class="card">
+            <h2 class="card-title"><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
+            <hr>
+            <?php if (metadata('item', 'has files')): ?>
+                <div class="card-img-top">
+                    <?php echo link_to_item(item_image('square_thumbnail')); ?>
                 </div>
             <?php endif; ?>
-            <?php if (metadata('item', 'has tags')): ?>
-                <div class="tags"><p><strong><?php echo __('Tags'); ?>:</strong>
+
+            <div class="card-body">
+                <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
+                    <div class="card-text item-description">
+                        <?php echo $description; ?>
+                    </div>
+                <?php endif; ?>
+                <?php if (metadata('item', 'has tags')): ?>
+                    <div class="tags"><p><strong><?php echo __('Tags'); ?>:</strong>
                         <?php echo tag_string('items'); ?></p>
-                </div>
-            <?php endif; ?>
-
+                    </div>
+                <?php endif; ?>
             <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
+            </div>
         </div>
+    <?php endforeach; ?>
     </div>
-
-<?php endforeach; ?>
-</div>
 </div>
 <div class="footer-controls">
     <div class="container">
