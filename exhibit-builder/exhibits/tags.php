7b88e459f76f65c835f9d620eaa767b5e4857d1f
Jonathan Candelaria <candelariajr@appstate.edu>
2020-12-11 12:01:22 -0500

    Update tags.php

--- exhibit-builder/exhibits/tags.php
+++ exhibit-builder/exhibits/tags.php
@@ -2,22 +2,35 @@
 $title = __('Browse Exhibits by Tag');
 echo head(array('title' => $title, 'bodyclass' => 'exhibits tags'));
 ?>
-<h1><?php echo $title; ?></h1>
-
-<nav class="navigation exhibit-tags">
-    <?php echo nav(array(
-            array(
-                'label' => __('Browse All'),
-                'uri' => url('exhibits/browse')
-            ),
-            array(
-                'label' => __('Browse by Tag'),
-                'uri' => url('exhibits/tags')
-            )
-        )
-    ); ?>
-</nav>
-
-<?php echo tag_cloud($tags, 'exhibits/browse'); ?>
-
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2><?php echo $title; ?></h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <?php echo tag_cloud($tags, 'exhibits/browse'); ?>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <?php echo nav(array(
+                    array(
+                        'label' => __('Browse All'),
+                        'uri' => url('exhibits/browse')
+                    ),
+                    array(
+                        'label' => __('Browse by Tag'),
+                        'uri' => url('exhibits/tags')
+                    )
+                )
+            ); ?>
+        </div>
+    </div>
+</div>
 <?php echo foot(); ?>
