2ca5522c6df7dc7cd69881345cdac43a12bfa3b8
Jonathan Candelaria <candelariajr@appstate.edu>
2022-09-29 13:32:16 -0400

    Update footer.php

--- common/footer.php
+++ common/footer.php
@@ -7,28 +7,28 @@ $navArray[] = array('label'=>'Contribute', 'uri'=>url('contribution'));
 ?>
 <?php //echo nav($navArray); ?>
 <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
-<div class="footer-wrapper">
+<div class="footer-wrapper" alt="Page Foot" title="Page Foot">
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-3">
-                © 2021 Appalachian State University Boone, NC 28608 | 828-262-2000
-                Proudly powered by <a class="asu-link" href="http://omeka.org">Omeka</a>
+                © 2022 Appalachian State University Boone, NC 28608 | 828-262-2000
+                Proudly powered by <a class="asu-link" alt="Omeka Platform Home (Outside Link)" title="Omeka Platform Home (Oustide Site)" href="https://omeka.org">Omeka</a> and Laminas MVC
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3">
                 <h1 class="footer-links-title">Links</h1>
                 <ul class="footer-links-menu">
-                    <li><a href="<?php echo url('items')?>">Browse Items</a></li>
+                    <li><a alt="Browse All Items" title="Browse All Items" href="<?php echo url('items')?>">Browse Items</a></li>
                     <!--   Had to hard wire this. For some reason it keeps overwriting this with a random URL -->
-                    <li><a href="<?php echo "https://omeka.library.appstate.eudu/collections"?>">Browse Collections</a></li>
-                    <li><a href="<?php echo url('contact')?>">Contact</a></li>
-                    <li><a href="<?php echo url('contribution')?>">Contribute</a></li>
+                    <li><a alt="Browse Collections" title="Browse Collections" href="<?php echo "https://omeka.library.appstate.eudu/collections"?>">Browse Collections</a></li>
+                    <li><a alt="Contact Us" title="Contact Us" href="<?php echo url('contact')?>">Contact</a></li>
+                    <li><a alt="Contribute" title="Contribute" href="<?php echo url('contribution')?>">Contribute</a></li>
                 </ul>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3">
 
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3 pull-right" id="footer-university">
-                <a href="http://appstate.edu" title="Appalacian State University">
+                <a alt="Appalachian State University Home" title="Appalachian State University Home" href="https://appstate.edu" title="Appalacian State University">
                     <img src="https://library.appstate.edu/profiles/asu/themes/custom/asu_theme/images/appstatelogo.png"
                          id="asu-footer-logo" class="pull-right" alt="Appalachian State University">
                 </a>
@@ -36,11 +36,10 @@ $navArray[] = array('label'=>'Contribute', 'uri'=>url('contribution'));
         </div>
         <div class="row">
             <div class="col">
-                <div style="font-size: 10px; color: grey;cursor: auto;">Architecture and UI adapted to AppState Theme by Jonathan Candelaria</div>
+                <div style="font-size: 10px; color: #404040 ;cursor: auto;">Architecture and UI adapted to AppState Theme by Jonathan Candelaria</div>
             </div>
         </div>
     </div>
 </div>
 </body>
 </html>
-~
