ff6dbfa39b396293362fd9ad07646dcdf084922a
Jonathan Candelaria <candelariajr@appstate.edu>
2022-10-25 21:09:57 -0400

    303 Display Error Fix

--- items/browse.php
+++ items/browse.php
@@ -16,11 +16,7 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse')); ?>
     </div>
 </div>
 <br>
-
-<?php echo item_search_filters(); ?>
-
 <?php if ($total_results > 0): ?>
-
 <?php
 $sortLinks[__('Title')] = 'Dublin Core,Title';
 $sortLinks[__('Creator')] = 'Dublin Core,Creator';
@@ -31,36 +27,33 @@ These are the sortlinks. They have been removed for some reason.
 <div id="sort-links">
     <span class="sort-label"><?php //echo __('Sort by: '); ?></span><?php //echo browse_sort_links($sortLinks); ?>
 </div>-->
-
 <?php endif; ?>
 <div class="container">
-    <div class="card-columns">
+    <?php echo item_search_filters(); ?>
     <?php foreach (loop('items') as $item): ?>
-        <div class="card">
-            <h2 class="card-title"><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
-            <hr>
-            <?php if (metadata('item', 'has files')): ?>
-                <div class="card-img-top">
-                    <?php echo link_to_item(item_image('square_thumbnail')); ?>
+        <div class="item">
+            <?php $itemTitle = strip_formatting(metadata('item', array('Dublin Core', 'Title'))); ?>
+            <?php if (metadata('item', 'has thumbnail')): ?>
+                <div class="item-img">
+                    <?php echo link_to_item(item_image('square_thumbnail', array('alt' => $itemTitle))); ?>
                 </div>
             <?php endif; ?>
-
-            <div class="card-body">
-                <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
-                    <div class="card-text item-description">
-                        <?php echo $description; ?>
+            <div class="item-info">
+                <h4><?php echo link_to_item($itemTitle, array('class'=>'permalink')); ?></h4>
+                <?php if ($itemDescription = metadata('item', array('Dublin Core', 'Description'))) :?>
+                    <div class="item-description">
+                        <?php echo $itemDescription; ?>
                     </div>
                 <?php endif; ?>
-                <?php if (metadata('item', 'has tags')): ?>
-                    <div class="tags"><p><strong><?php echo __('Tags'); ?>:</strong>
-                        <?php echo tag_string('items'); ?></p>
+                <?php if($tags = tag_string('item', 'items')): ?>
+		    <div>
+                        <p class="tags"><?php echo __('Tags: ') . $tags ?></p>
                     </div>
                 <?php endif; ?>
             <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
             </div>
         </div>
     <?php endforeach; ?>
-    </div>
 </div>
 <div class="footer-controls">
     <div class="container">
